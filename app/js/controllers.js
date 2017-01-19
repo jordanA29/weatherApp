bbfeccfd00329f1027ec8f0091c6fef64ca52145
C83 <cy1cy@laposte.net>
2017-01-19 18:12:55 +0100

    Add temperature list

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,18 +5,33 @@
   controller('brestController', function($scope,$routeParams,$http) {
   	$scope.result = {};
     $http.get('assets/json/data.json').success(function(data) {
-        $scope.result = data;
-        console.log($scope.result);  
-         for(city of $scope.result.cities){
-    		if(city.name =="Brest"){
-  				$scope.cityData = city;
-   				console.log($scope.cityData);      		
-    		}
-    	}
-    	$scope.brCurrentTemp = $scope.cityData.forecast[0].current.temp;
-    	console.log($scope.brCurrentTemp); 
+      $scope.result = data;
+      console.log($scope.result);  
+      for(city of $scope.result.cities){
+        if(city.name =="Brest"){
+          $scope.cityData = city;
+          console.log($scope.cityData);      		
+        }
+      }
+      $scope.brCurrentTemp = $scope.cityData.forecast[0].current.temp;
+      console.log($scope.brCurrentTemp); 
     });
- 
+
+    $scope.detailTempVisibled = false;
+    $scope.detailWindVisibled = false;
+    $scope.detailPrecVisibled = false;
+    $scope.valDay = 0;
+    $scope.result;
+    $scope.showTempDetails = function(){
+      $scope.detailTempVisibled = !$scope.detailTempVisibled;
+    }
+    $scope.showWindDetails = function(){
+      $scope.detailWindVisibled = !$scope.detailWindVisibled;
+    }
+    $scope.showPrecDetails = function(){
+      $scope.detailPrecVisibled = !$scope.detailPrecVisibled;
+    }
+    
 
   }).
 
